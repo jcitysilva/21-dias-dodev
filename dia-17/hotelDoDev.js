5f3886a7b29136f30fe862dd612142ca0cdedbda
jcitysilva <joaosi2010@gmail.com>
2024-05-18 01:15:07 +0100

    Finalizado exercício hotelDoDev

--- dia-17/hotelDoDev.js
+++ dia-17/hotelDoDev.js
@@ -90,17 +90,64 @@ function RegistaReserva() {
 // --> Dia de entrada;
 // --> Dia de saída.
 
+// FUNÇÃO PARA PROCURAR RESERVAS PELO HOTEL
+function ProcurarReservasPeloHotel(idHotel) {
+    reservas.forEach(reserva => {
+        if (idHotel == reserva.IdHotel) {
+            let i = idHotel -1
+            console.log("hotel:", hoteis[i].Nome)
+            console.log("responsável:", reserva.Responsavel)
+            console.log("Dia de entrada:", reserva.DiaEntrada)
+            console.log("Dia de saída:", reserva.DiaSaida)
+        }
+    })
+}
+
 // 4 Função que recebe como parâmetro o id de reserva e exibe na console:
 // --> Nome do hotel;
 // --> Endereço
 // --> Dia de entrada;
 // --> Dia de saída.
 
+// FUNÇÃO PARA PROCURAR HOTEL PELA RESERVA
+function ProcurarHotelPelaReserva(idReserva) {
+    let idHotel = reservas[idReserva - 1].IdHotel
+    console.log("hotel:", hoteis[idHotel -1 ].Nome)
+    console.log("endereço:", hoteis[idHotel - 1].Endereco)
+    console.log("dia de entrada:", reservas[idReserva - 1].DiaEntrada)
+    console.log("dia de saída:", reservas[idReserva - 1].DiaSaida)
+}
+
 // 5 Cria uma função recebe nome de pessoa e exibe todas as suas reservas
+function ProcurarReservaPeloNome(nome) {
+    for (let i = 0; i < reservas.length; i++) {
+        if (nome == reservas[i].Responsavel) {
+            console.log("id da Reserva: " + reservas[i].Id)
+            console.log("hotel: " + hoteis[(reservas[i].IdHotel) - 1].Nome)
+        }
+    }
+}
 
 // 6 Cria uma função que recebe uma categoria e retorna um array com todos os hotéis nessa categoria
 
+// FUNÇÃO PARA PROCURAR HOTÉIS PELA CATEGORIA
+function ProcurarHotelPelaCategoria(categoria) {
+    let hoteisProcurados = []
+    for (let i = 0; i < hoteis.length; i++) {
+        if (categoria == hoteis[i].Categoria) {
+            hoteisProcurados.push(hoteis[i].Nome)
+        }
+    }
+    return hoteisProcurados
+}
+
 // 7 Cria função que recebe o id de um hotel e um telefone como parâmetro, a função deve atualizar o telefone com o número recebido
+
+// FUNÇÃO PARA ATUALIZAR TELEFONE DO HOTEL
+function AtualizarTelefone(idHotel, telefone) {
+    hoteis[idHotel - 1].Telefone = telefone
+    console.log("Número de telefone actualizado!")
+}
 
 // 8 Cria um fluxo de funcionamento para o algoritmo, o utilizador escolhe quando encerrar o programa
 
@@ -109,3 +156,44 @@ function RegistaReserva() {
 // --> O dia de entrada não pode ser maior que o dia de saída;
 // --> No registo de uma reserva o id do hotel deve ser válido, ou seja, não deve permitir o registo de um hotel que não esteja no sistema;
 
+// FLUXO DE FUNCIONAMENTO
+let continuar = true
+do {
+    let opcao = prompt("escolhe uma opção: \n1 - Registar hotel \n2 - Registar reserva \n3 - Procurar reserva pelo hotel" +
+        "\n4 - Procurar hotel pela reserva \n5 - Procurar reserva pelo responsável \n6 - Procurar hotéis por categoria" +
+        "\n7 - Atualizar telefone de um hotel \n8 - encerrar o programa")
+
+    switch (opcao) {
+        case "1":
+            RegistaHotel()
+            break;
+        case "2":
+            RegistaReserva()
+            break;
+        case "3":
+            ProcurarReservasPeloHotel(prompt("Digite o id do hotel"))
+            break;
+        case "4":
+            ProcurarHotelPelaReserva(prompt("Digite o id da reserva"))
+            break;
+        case "5":
+            ProcurarReservaPeloNome(prompt("Digite o nome do responsável pela reserva"))
+            break;
+        case "6":
+            let hoteisProcurados = ProcurarHotelPelaCategoria(parseInt(prompt("Digite a categoria que deseja procurar")))
+            console.log(hoteisProcurados)
+            break;
+        case "7":
+            let idHotel = parseInt(prompt("Digite o id do hotel que deseja atualizar"))
+            let telefone = prompt("digite o novo telefone")
+            AtualizarTelefone(idHotel, telefone)
+            break
+        case "8":
+            console.log("Programa encerrado")
+            continuar = false
+            break;
+        default:
+            console.log("opção inválida")
+            break;
+    }
+} while (continuar)
